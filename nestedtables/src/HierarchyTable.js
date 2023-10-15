3eb6a503c0519199b0737cf6e830c97266b83f19
David Truhlar <truhlik24@gmail.com>
2023-10-15 10:34:08 +0200

    Adds skeleton of hierarchytable.js

---
+++ nestedtables/src/HierarchyTable.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function HierarchyTable({ data }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Attribute</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+          <tr test>
+            <td>8</td>
+            <td>Lol</td>
+          </tr>
+        
+      </tbody>
+    </table>
+  );
+}
+
+export default HierarchyTable;
--- nestedtables/src/index.js
+++ nestedtables/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HierarchyTable from './HierarchyTable';
 import './index.css';
 import jsonData from './data.json';
 
 function App() {
     return (
-      <div>
-        <h1>Hello Wolrd</h1>
-      </div>
+    <div>
+        <h1>Hierarchical Table</h1>
+        <HierarchyTable data={jsonData} />
+    </div>
     );
   }
   
-  ReactDOM.render(
+ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
     document.getElementById('root')
-  );
+);
